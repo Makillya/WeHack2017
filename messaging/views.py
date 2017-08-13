4134179cbc93d9902528eadcc18e72fc7fca782c
liuyishui <30963771+liuyishui@users.noreply.github.com>
2017-08-13 13:40:53 -0700

    Update views.py

--- messaging/views.py
+++ messaging/views.py
@@ -12,3 +12,13 @@ def addMessage(request,addresseeId):
         creatorId=request.session["currentUser"]
         message=Message.objects.add(request.POST, creatorId, addresseeId)
     return redirect(request, "messaging:index")
+
+def chatHist(request, rec):
+    currentID = request.session['currentUser'] #get current user id
+    if currentID == None or rec == None:
+        return None
+    data = Message.objects.filter(creator.id=currentID).filter(addressee.id=rec)
+    data = data.join(Message.objects.filter(creator.id=rec).filter(addressee.id=currentID)).sort_by('created_at')
+    response = HttpResponse(data, content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="result.csv"' 
+    return response
