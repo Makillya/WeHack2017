4b5b4c29185512d868d429a96d99d6ebc43e1f37
Daniela Bizgu <bizgudaniela1@gmail.com>
2017-08-13 10:33:16 -0700

    AddMessage Function

--- messaging/views.py
+++ messaging/views.py
@@ -4,4 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'loginreg/index.html')
+    return render(request, 'messaging/index.html')
+
+# Add message to DB
+def addMessage(request,addresseeId):
+    if request.method == "POST":
+        creatorId=request.session["currentUser"]
+        message=Message.objects.add(request.POST, creatorId, addresseeId)
+    return redirect(request, "messaging:index")
