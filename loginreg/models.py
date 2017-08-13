66d58d23c4b880e749657f5ed2e8c40194eac3e2
Daniela Bizgu <bizgudaniela1@gmail.com>
2017-08-12 23:29:05 -0700

    Addded logig for adding a user

--- loginreg/models.py
+++ loginreg/models.py
@@ -25,7 +25,9 @@ USERNAME_REGEX = re.compile(r'^[a-zA-Z0-9]([._](?![._])|[a-zA-Z0-9]){5,18}[a-zA-
 # numbers.
 DATE_REGEX = re.compile(r'^[0-9]{4}-[0-9]{2}-[0-9]{2}$')
 
-class mentorManager(models.Manager):
+
+
+class UserManager(models.Manager):
     # Call this function to check for validation of all field for registation.
     # How to call: Table.userManager.registration(request.POST)
     def registerMentor(self, userInput):
@@ -67,7 +69,7 @@ class mentorManager(models.Manager):
         # If the list has no errors.
         if not errorList:
             hashed = bcrypt.hashpw(userInput['password'].encode(), bcrypt.gensalt())
-            current_user = self.create(first_name = userInput['first_name'], last_name = userInput['last_name'], email = userInput['email'], password = hashed)
+            current_user = self.create(first_name = userInput['first_name'], last_name = userInput['last_name'], user_type=userInput["user_type"], email = userInput['email'], password = hashed)
             return True, current_user
         return False, errorList
 
@@ -85,11 +87,6 @@ class mentorManager(models.Manager):
         elif self.filter(email = userInput['email']) :
             hashed = self.get(email = userInput['username_email']).password.encode()
             password = userInput['password'].encode()
-            # Check to see if user is in Mentee table
-        elif Mentee.menteeManager.filter(email = userInput['email']):
-            hashed = Mentor.mentorManager.get(email = userInput['email']).password.encode()
-            password = userInput['password'].encode()
-
             # Checks if the password is the correct one to the hashed one.
             if bcrypt.hashpw(password, hashed) == hashed:
                 return True, self.get(email=userInput['email'])
@@ -97,73 +94,20 @@ class mentorManager(models.Manager):
             else:
                 errorList.append('Unsuccessful login. Incorrect password!\n')
         else:
-            errorList.append('Unsuccessful login. Your email or username is incorrect!\n')
+            errorList.append('Unsuccessful login. Your email is incorrect!\n')
         return False, errorList
 
-
-class menteeManager(models.Manager):
-    # Call this function to check for validation of all field for registation.
-    # How to call: Table.userManager.registration(request.POST)
-    def registerMentee(self, userInput):
-        # errorList - Keeps tracks of all errors with the validation.
-        errorList = []
-
-        # Checks to see if first_name is greater than 2 characters.
-        if len(userInput['first_name']) < 2:
-            errorList.append('First name needs to be greater than 2 letters!\n')
-
-        # Checks to see if last_name is greater than 2 characters.
-        if len(userInput['last_name']) < 2:
-            errorList.append('Last name needs to be greater than 2 letters!\n')
-
-        # Checks to see if first_name contains only alphabetical letters.
-        if not userInput['first_name'].isalpha():
-            errorList.append('First name may only contain letters!\n')
-
-        # Checks to see if last_name contains only alphabetical letters.
-        if not userInput['last_name'].isalpha():
-            errorList.append('Last name may only contain letters!\n')
-
-        # Checks if the email matches the EMAIL_REGEX.
-        if not EMAIL_REGEX.match(userInput['email']):
-            errorList.append('Email is not a valid email! Try this format: something@example.com\n')
-
-        # Checks if the password matches the PASSWORD_REGEX.
-        if not PASSWORD_REGEX.match(userInput['password']):
-            errorList.append('Password is not long enough.\n')
-
-        # Checks to make sure the password inputs match.
-        if userInput['password'] != userInput['confirm_password']:
-            errorList.append('Password match not confirmed.\n')
-
-        # Checks the database to see if the email already exists.
-        if self.filter(email = userInput['email']):
-            errorList.append('This email already exists in our database.\n')
-
-        # If the list has no errors.
-        if not errorList:
-            hashed = bcrypt.hashpw(userInput['password'].encode(), bcrypt.gensalt())
-            current_user = self.create(first_name = userInput['first_name'], last_name = userInput['last_name'], email = userInput['email'], password = hashed)
-            return True, current_user
-        return False, errorList
-
-
-
-
-
-
-
-
-
-
-
-class UserManager(models.Manager):
-    def testy(self):
-        pass
 
 class RelationshipManager(models.Manager):
-    def test(self):
-        pass
+    def add_relationship(self, user, status, symm=True):
+        relationship, created = Relationship.objects.get_or_create(
+            from_user=self,
+            to_user=user,
+            status=status)
+        if symm:
+            # avoid recursion by passing `symm=False`
+            user.add_relationship(self, status, False)
+        return relationship
 
 class User(models.Model):
     first_name = models.CharField(max_length=25)
@@ -195,6 +139,5 @@ RELATIONSHIP_STATUSES = (
 class Relationship(models.Model):
     from_user = models.ForeignKey(User, related_name='from_people')
     to_user = models.ForeignKey(User, related_name='to_people')
-    status = models.IntegerField(choices=RELATIONSHIP_STATUSES)
+    status = models.BooleanField(default=False)
     objects = RelationshipManager()
-    
