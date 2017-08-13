b01a9552dfb119ba5eaef6cf3ec453045ea4062f
Daniela Bizgu <bizgudaniela1@gmail.com>
2017-08-13 14:28:19 -0700

    LOGIN REGISTER WORKING IUHUUUUU

--- loginreg/views.py
+++ loginreg/views.py
@@ -1,37 +1,38 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from .models import User
+from django.contrib import messages
 
 def index(request):
     return render(request, 'loginreg/index.html')
 
 # Register user as MENTOR or MENTEE
 def register(request):
+    print "**********"
     if request.method == 'POST':
         userInput=request.POST
-        # If MENTOR
-        if userInput["userType"] == "mentor":
-            user = User.UserManager.register(request.POST)
-            if not user[0]:
-                for i in range(0, len(user[1])):
-                    messages.error(request, user[1][i])
-                return redirect ('loginreg:index')
-            else:
-                request.session['currentUser'] = user[1].id
-                return redirect('loginreg:profile')
+        user = User.objects.register(request.POST)
+        if not user[0]:
+            for i in range(0, len(user[1])):
+                messages.error(request, user[1][i])
+            return redirect ('loginreg:index')
+        else:
+            request.session['currentUser'] = user[1].id
+            return redirect('loginreg:profile')
 
 
 # Login function
 def login(request):
-    user = User.UserManager.login(request.POST)
+    user = User.objects.login(request.POST)
     if not user[0]:
         for i in range(0, len(user[1])):
             messages.error(request, user[1][i])
         return redirect('loginreg:index')
     else:
         request.session['currentUser'] = user[1].id
-        return redirect ('loginreg:success')
+        return redirect ('loginreg:profile')
 
 
 # Logout function
@@ -43,5 +44,5 @@ def logout(request):
 def success(request):
     return null
 
-def profile(arg):
-    pass
+def profile(request):
+    return render(request, 'loginreg/profile.html')
