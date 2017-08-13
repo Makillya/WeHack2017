0beacef351c69b4ebe226b5e8fd1eb0fa26f00bc
Mikayla Jordan <hippypanda@gmail.com>
2017-08-12 21:46:28 -0700

    added admin user

--- loginreg/admin.py
+++ loginreg/admin.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Mentor, Mentee
+
+admin.site.register(Mentor)
+admin.site.register(Mentee)
+
 # Register your models here.
