07b10cbacca2c4a5828c55eee214c0615c9d551e
Mikayla Jordan <hippypanda@gmail.com>
2017-08-13 10:30:08 -0700

    created message models

--- messaging/models.py
+++ messaging/models.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from loginreg.models import User
 
-# Create your models here.
+class MessageManager(models.Manager):
+    def test(self):
+        pass
+
+class Message(models.Model):
+    content = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    addressee = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    objects = MessageManager()
