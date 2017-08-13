ab3239ed0b0e704ba8419b09c6a27c5d498f50cb
Mikayla Jordan <hippypanda@gmail.com>
2017-08-12 23:02:20 -0700

    reconfigured models for single users table

--- loginreg/admin.py
+++ loginreg/admin.py
@@ -3,9 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Mentor, Mentee
+from .models import User
 
-admin.site.register(Mentor)
-admin.site.register(Mentee)
+admin.site.register(User)
 
-# Register your models here.
